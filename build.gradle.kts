0e136d0ba3e07394b6572e205fc520ed25bc3d6e
Eddy Escobar <35607609+hydrohd@users.noreply.github.com>
2024-02-06 18:11:54 -0700

    maybe

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
     `maven-publish`
 }
 
-group = "com.github.hydrohd.lolesportsapikt"
+group = "com.github.hydrohd"
 version = "master-SNAPSHOT"
 
 publishing {
     publications{
         create<MavenPublication>("lolesportsapikt"){
-            groupId = "com.github.hydrohd.lolesportsapikt"
+            groupId = "com.github.hydrohd"
             artifactId = project.name
             from(components["java"])
         }
@@ -22,18 +22,18 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val ktor_version: String by project
-val logback_version: String by project
+val ktorVersion: String by project
+val logbackVersion: String by project
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("io.ktor:ktor-client-auth:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-client-logging:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-auth:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
 
 }
 
